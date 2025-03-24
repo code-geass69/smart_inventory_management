798763fe5f9f198d8857e55e16f4bba4ab9f6381
Atharv More <atharvmore30@gmail.com>
2025-03-24 09:23:16 +0530

    Project started by Atharv

---
+++ src/db/index.ts
@@ -0,0 +1,7 @@
+import * as schema from "@/db/schema";
+import { env } from "@/env.mjs";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const pool = new Pool({ connectionString: env.DATABASE_URI });
+export const db = drizzle(pool, { schema });
