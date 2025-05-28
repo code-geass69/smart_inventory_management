776d982a092f3a2b8c94d3318039dece66a88106
Atharv More <atharvmore30@gmail.com>
2025-05-28 23:36:57 +0530

    modified:   src/app/api/dashboard/top-selling-items/route.ts

--- src/app/api/dashboard/top-selling-items/route.ts
+++ src/app/api/dashboard/top-selling-items/route.ts
@@ -6,19 +6,19 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const topSellingItemsResult = await db
-      .select([
-        items.id,
-        items.name,
-        sql`SUM(${orderItems.quantity}) AS total_quantity_sold`,
-        sql`SUM(${orderItems.quantity} * ${orderItems.price}) AS total_price`
-      ])
+      .select({
+        id: items.id,
+        name: items.name,
+        total_quantity_sold: sql`SUM(${orderItems.quantity})`.as("total_quantity_sold"),
+        total_price: sql`SUM(${orderItems.quantity} * ${orderItems.price})`.as("total_price"),
+      })
       .from(orderItems)
       .innerJoin(items, sql`${items.id} = ${orderItems.itemId}`)
       .groupBy(items.id)
-      .orderBy(sql`total_quantity_sold`)
+      .orderBy(sql`total_quantity_sold DESC`)
       .limit(10)
       .execute();
-      
+
     return NextResponse.json({
       status: "success",
       data: topSellingItemsResult,
